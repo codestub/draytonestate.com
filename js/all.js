96f3c75040f62604e439fbc6771889836aaf8a14
Seb <sebrao@live.co.uk>
2016-08-24 15:05:54 -0500

    Fixed things
    Adding in a Jquery toggle menu broke everything, so I recreated the
    entire menu system, because it was completely fucked. Added in a slick
    slider and fixed responsive layouts. Also changed slider images and
    photoshopped them a wee bit

--- js/all.js
+++
@@ -1,7 +0,0 @@
-function toggle_visibile(id) {
-  var element = document.getElementById(id);
-  if(element.style.display == 'none')
-    element.style.display = 'block';
-  else
-    element.style.display = 'none';
-}
